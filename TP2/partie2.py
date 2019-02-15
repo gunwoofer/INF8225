06bcb4a3f5908ac1c8c0bdc4b3343cb67ebdf99d
Valentin <valentin.bouis@gmail.com>
2019-02-15 16:55:09 -0500

    testing

--- TP2/partie2.py
+++ TP2/partie2.py
@@ -5,13 +5,15 @@ from cnn import CNN
 import torch.nn as nn
 from torchvision import transforms
 from torch.autograd import Variable
+import matplotlib.pyplot as plt
 
 print ("TP2 INF8225..")
 
+# Source : https://mc.ai/intro-to-pytorch-with-image-classification-on-a-fashion-clothes-dataset/
 
 # Global setup
 nb_epoch = 5
-batch_size = 100
+batch_size = 80
 lr = 0.001
 
 # Download the MNIST dataset
@@ -19,7 +21,7 @@ transform = transforms.Compose([
     transforms.ToTensor()
 ])
 mnist_trainset = datasets.MNIST(root="./mnist", train=True, download=True, transform=transform)
-mnist_testset = datasets.MNIST(root="./mnist", train=False, download=True, transform=None)
+mnist_testset = datasets.MNIST(root="./mnist", train=False, download=True, transform=transform)
 
 # Load the dataset
 train_loader = torch.utils.data.DataLoader(dataset=mnist_trainset,batch_size=batch_size,shuffle=True)
@@ -30,14 +32,13 @@ cnn = CNN()
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(cnn.parameters(), lr=lr)
 
+
+
 # Training
 losses = []
 for epoch in range(nb_epoch):
-    for i, (images, labels) in enumerate(train_loader):
-        images = Variable(images.float())
-        labels = Variable(labels)
-
-        # Forward + Backward + Optimize
+    print("epoch " + str(epoch) + "...")
+    for images, labels in train_loader:
         optimizer.zero_grad()
         outputs = cnn(images)
         loss = criterion(outputs, labels)
@@ -46,7 +47,21 @@ for epoch in range(nb_epoch):
 
         losses.append(loss.item())
 
-        if (i+1) % 100 == 0:
-            print ('Epoch : %d/%d, Iter : %d/%d,  Loss: %.4f' 
-                   %(epoch+1, nb_epoch, i+1, len(mnist_trainset)//batch_size, loss.item()))
+
+# Testing
+correct = 0
+for images, labels in test_loader:
+    outputs = cnn(images)
+    predict = torch.argmax(outputs, 1)
+    correct = correct + (predict == labels).sum()
+    
+
+print ("Precision sur le test set : ", correct.item() / test_loader.sampler.num_samples * 100)
+
+
+
+# Plot
+plt.plot(losses[0::500])
+plt.show()
+
 print ("DONE")
